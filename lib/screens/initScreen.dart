544f866fa6ef4a9124d56362a591bd5769730deb
eduardoyuki <yukieduardo1@gmail.com>
2023-11-08 21:52:45 -0300

    Fazendo a tela de cadastro e login.2

--- lib/screens/initScreen.dart
+++ lib/screens/initScreen.dart
@@ -20,12 +20,20 @@ class _InitScreenState extends State<InitScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.greenAccent,
+      color: Colors.teal[900],
       child: Column(
         children: <Widget>[
           Container(
             height: displayHeight() / 3,
-            color: Colors.greenAccent[100],
+            decoration: BoxDecoration(
+              color: Colors.teal[800],
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(80),
+                bottomRight: Radius.circular(80),
+              )
+            ),
+            child: Icon(Icons.person, color: Colors.white, size:50,),
+
           )
         ],
       )
