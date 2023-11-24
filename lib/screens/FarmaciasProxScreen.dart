b0c3ff79d0ec7cb40ad9a43189098f76971ab54f
IsaacBTN <isaacbertonha3@gmail.com>
2023-11-24 16:58:16 -0300

    Adição Mapa - Farmacias Proximas

--- lib/screens/FarmaciasProxScreen.dart
+++ lib/screens/FarmaciasProxScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:geolocator/geolocator.dart';
 
 class FarmaciasProxScreen extends StatelessWidget {
   @override
@@ -7,9 +9,63 @@ class FarmaciasProxScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Farmácias Próximas'),
       ),
-      body: Center(
-        child: Text('Conteúdo da tela de Farmácias Próximas'),
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Color(0xFF00695C), // Cor de fundo #00695c
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // Ícone de mapa (place) maior
+            Icon(
+              Icons.place,
+              color: Colors.white,
+              size: 80.0,
+            ),
+
+            // Botão para buscar farmácias próximas no Google Maps
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 20),
+              child: ElevatedButton(
+                onPressed: () {
+                  _openGoogleMapsForPharmacies();
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.teal[900], // Cor de fundo
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Text(
+                    'Buscar Farmácias Próximas',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  // Função para abrir o Google Maps e pesquisar farmácias próximas
+  void _openGoogleMapsForPharmacies() async {
+    String searchQuery = 'farmácias próximas'; // Pesquisa por farmácias próximas
+    String googleMapsUrl = 'https://www.google.com/maps/search/$searchQuery';
+
+    // Verifica se o URL pode ser lançado
+    if (await canLaunch(googleMapsUrl)) {
+      await launch(googleMapsUrl);
+    } else {
+      // Se não for possível lançar, exiba uma mensagem de erro
+      print('Erro ao abrir o Google Maps');
+    }
   }
 }
