cee5116f94e2ecda4176e0ca00daff728776c314
eduardoyuki <yukieduardo1@gmail.com>
2023-12-03 19:34:08 -0300

    add a função que chama a api de sms, e add o ícone do app

--- lib/screens/AceiteScreen.dart
+++ lib/screens/AceiteScreen.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:medfinderflutter/screens/menu.dart';
+import 'dart:convert';
+import 'dart:async';
 
-class AceiteScreen extends StatelessWidget {
+class AceiteScreen extends StatefulWidget {
+  @override
+  _AceiteScreenState createState() => _AceiteScreenState();
+}
+
+class _AceiteScreenState extends State<AceiteScreen> {
+  bool _aceiteRealizado = false;
+  Timer? _timer;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,7 +25,7 @@ class AceiteScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Voce tomou seu medicamento?',
+              'Você tomou seu medicamento?',
               style: TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
@@ -22,10 +34,13 @@ class AceiteScreen extends StatelessWidget {
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                // Adicione a ação desejada para o botão central
+                _pararTimer();
+                _realizarAceite();
+                _enviarSolicitacao();
+                _navegarParaMenu();
               },
               style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.teal[900], // Cor de fundo do botão
+                backgroundColor: Colors.teal[900],
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(30),
                 ),
@@ -41,5 +56,54 @@ class AceiteScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _realizarAceite() {
+    setState(() {
+      _aceiteRealizado = true;
+    });
+  }
+
+  void _enviarSolicitacao() async {
+    final url = Uri.parse('http://localhost:3030/send');
+
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'message': 'Medicamento Tomado', 'number': 'seu-numero'}),
+    );
+
+    // Não é necessário mais retornar para a tela do menu aqui
+    // Navigator.pop(context);
+  }
+
+  void _pararTimer() {
+    if (_timer != null && _timer!.isActive) {
+      _timer!.cancel();
+    }
+  }
+
+  void _navegarParaMenu() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => MenuScreen()),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer(Duration(minutes: 30), () {
+      if (!_aceiteRealizado) {
+        _enviarSolicitacao();
+        _navegarParaMenu();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _pararTimer();
+    super.dispose();
   }
 }
